42a9a00c91d7452811f1461e96255bf67b21eea9
Jeffrey Lee <darkthread@gmail.com>
2021-05-01 23:53:53 +0800

    support esp32s w SPIFFS

--- WebOnFS/src/main.cpp
+++ WebOnFS/src/main.cpp
@@ -3,34 +3,50 @@
 #include <Adafruit_SSD1306.h>
 #include <ESPAsyncWebServer.h>
 #include "Guineapig.WiFiConfig.h"
+#ifdef ESP8266
+#define WebFS LittleFS
 #include "LittleFS.h"
+#define USE_LITTLEFS
+#else
+#define WebFS SPIFFS
+#include <FS.h>
+#include <SPIFFS.h>
+#endif
 
 //參考 https://swf.com.tw/?p=905
 //參考 https://github.com/me-no-dev/ESPAsyncWebServer
 //參考 https://github.com/khoih-prog/ESPAsync_WiFiManager
 //參考 https://randomnerdtutorials.com/esp32-web-server-spiffs-spi-flash-file-system/
 
+#ifdef ESP8266
 #define LED_ON LOW
 #define LED_OFF HIGH
+#else
+#define LED_ON HIGH
+#define LED_OFF LOW
+#endif
 
-void saveStatus(String key, String value) {
-  auto f = LittleFS.open("/status/" + key, "w");
+void saveStatus(String key, String value)
+{
+  auto f = WebFS.open("/status/" + key, "w");
   f.print(value);
   f.close();
 }
-String readStatus(String key) {
-  if (!LittleFS.exists("/status/" + key)) return "";
-  auto f = LittleFS.open("/status/" + key, "r");
+String readStatus(String key)
+{
+  if (!WebFS.exists("/status/" + key))
+    return "";
+  auto f = WebFS.open("/status/" + key, "r");
   auto v = f.readString();
   f.close();
   return v;
 }
 
-const char* ledKey = "led_builtin";
+const char *ledKey = "led_builtin";
 
 AsyncWebServer server(80);
 
-#define SCREEN_ADDRESS 0x3C 
+#define SCREEN_ADDRESS 0x3C
 Adafruit_SSD1306 display(128, 64, &Wire, -1);
 void printOled(String msg)
 {
@@ -41,8 +57,20 @@ void printOled(String msg)
 void setup()
 {
   Serial.begin(115200);
-  delay(2000);
-    // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+#ifdef USE_LITTLEFS 
+  if (!WebFS.begin())
+  {
+    Serial.println("LittleFS Mount Failed");
+    return;
+  }
+#else  
+  if (!WebFS.begin(false))
+  {
+    Serial.println("SPIFFS Mount Failed");
+    return;
+  }
+#endif
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
   if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS))
   {
     Serial.println(F("SSD1306 allocation failed"));
@@ -57,19 +85,21 @@ void setup()
   display.setTextColor(WHITE, BLACK);
   display.setCursor(0, 56);
   display.ttyPrintln();
-  WiFiConfig.logCallback = printOled;
-
+  WiFiConfig.logCallback = printOled;  
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, (readStatus(ledKey) == "Y" ? LED_ON : LED_OFF));
   if (WiFiConfig.connectWiFi())
   {
+    auto staticWebHandler =
+        server.serveStatic("/html", WebFS, "/wwwroot/")
+            .setDefaultFile("index.html");  
     server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
       return request->redirect("/html/");
     });
     server.on("/led/status", HTTP_GET, [](AsyncWebServerRequest *request) {
-      request->send(200, "application/javascript", 
-        String("vm.LedSwitch=") + (readStatus(ledKey) == "Y" ? "true" : "false"));
-    });    
+      request->send(200, "application/javascript",
+                    String("vm.LedSwitch=") + (readStatus(ledKey) == "Y" ? "true" : "false"));
+    });
     server.on("/led/switch", HTTP_POST, [](AsyncWebServerRequest *request) {
       auto ledOn = request->arg("v") == "on";
       //Save LED status to file
@@ -77,21 +107,10 @@ void setup()
       saveStatus(ledKey, ledOn ? "Y" : "N");
       request->send(200, "text/plain", "OK");
     });
-    //啟動 LittleFS
-    LittleFS.begin();
-    //將 /wwwroot 對映到網址 /html
-    auto staticWebHandler = 
-      server.serveStatic("/html", LittleFS, "/wwwroot")
-            .setDefaultFile("index.html");
-    Dir dir = LittleFS.openDir("/wwwroot/js");
-    while (dir.next()) {
-      display.ttyPrintln(dir.fileName());
-    }
-    display.display();
     //網站可設定帳號密碼
     //staticWebHandler.setAuthentication(webUserId.c_str(), webUserPasswd.c_str());
     server.onNotFound([](AsyncWebServerRequest *request) {
-        request->send(404);
+      request->send(404);
     });
     server.begin();
   }
